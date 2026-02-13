63bb761ad29fbfda16227c0a4ef4fefd5dd80c22
uwe-mayer <uwe.mayer01@sap.com>
2026-02-13 15:01:35 +0100

    use CreateOrUpdate instead of custom logic

--- controller/shoot/rbac.go
+++ controller/shoot/rbac.go
@@ -7,12 +7,11 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/google/go-cmp/cmp"
 	corev1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
 
 const (
@@ -21,7 +20,6 @@ const (
 
 // SetRBAC ensures that the necessary RBAC permissions are set for the Greenhouse controller to operate on the shoot cluster.
 // This currently defaults to cluster-admin permissions
-// If an existing ClusterRoleBinding with our naming exists, we compare it with the desired state and recreate if different.
 // TODO: expose possibility to spec finegrained permissions for the Greenhouse controller
 func (r *ShootController) SetRBAC(ctx context.Context, shootClient client.Client, shootName string) {
 	greenhouseOrg := r.CareInstruction.GetNamespace()
@@ -29,82 +27,42 @@ func (r *ShootController) SetRBAC(ctx context.Context, shootClient client.Client
 		ObjectMeta: metav1.ObjectMeta{
 			Name: "greenhouse:system:cluster-admin",
 		},
-		Subjects: []rbacv1.Subject{
+	}
+
+	op, err := controllerutil.CreateOrUpdate(ctx, shootClient, clusterRoleBinding, func() error {
+		// Set the desired state
+		clusterRoleBinding.Subjects = []rbacv1.Subject{
 			{
 				Kind:     "User",
 				APIGroup: "rbac.authorization.k8s.io",
 				Name:     fmt.Sprintf(greenhouseSATemplate, greenhouseOrg, shootName),
 			},
-		},
-		RoleRef: rbacv1.RoleRef{
+		}
+		clusterRoleBinding.RoleRef = rbacv1.RoleRef{
 			APIGroup: "rbac.authorization.k8s.io",
 			Kind:     "ClusterRole",
 			Name:     "cluster-admin",
-		},
+		}
+		return nil
+	})
+
+	if err != nil {
+		r.Error(err, "failed to create or update ClusterRoleBinding for Greenhouse ServiceAccount", "ClusterRoleBinding", clusterRoleBinding.Name)
+		r.emitEvent(r.CareInstruction, corev1.EventTypeWarning, "RBACOperationFailed",
+			fmt.Sprintf("Failed to create or update ClusterRoleBinding %s for shoot %s/%s: %v", clusterRoleBinding.Name, r.CareInstruction.Namespace, shootName, err))
+		return
 	}
-	if err := shootClient.Create(ctx, clusterRoleBinding); err != nil {
-		if !apierrors.IsAlreadyExists(err) {
-			r.Error(err, "failed to create ClusterRoleBinding for Greenhouse ServiceAccount", "ClusterRoleBinding", clusterRoleBinding.Name)
-			r.emitEvent(r.CareInstruction, corev1.EventTypeWarning, "RBACCreationFailed",
-				fmt.Sprintf("Failed to create ClusterRoleBinding %s for shoot %s/%s: %v", clusterRoleBinding.Name, r.CareInstruction.Namespace, shootName, err))
-			return
-		}
 
-		// ClusterRoleBinding already exists - get it and compare
-		r.Info("ClusterRoleBinding already exists, comparing with desired state", "ClusterRoleBinding", clusterRoleBinding.Name, "shoot", shootName)
-
-		existingCRB := &rbacv1.ClusterRoleBinding{}
-		if err := shootClient.Get(ctx, client.ObjectKey{Name: clusterRoleBinding.Name}, existingCRB); err != nil {
-			r.Error(err, "failed to get existing ClusterRoleBinding", "ClusterRoleBinding", clusterRoleBinding.Name)
-			return
-		}
-
-		// Compare RoleRef and Subjects
-		needsRecreate := false
-
-		// Compare RoleRef
-		if existingCRB.RoleRef != clusterRoleBinding.RoleRef {
-			r.Info("RoleRef differs, will recreate",
-				"existing", fmt.Sprintf("%s/%s", existingCRB.RoleRef.Kind, existingCRB.RoleRef.Name),
-				"desired", fmt.Sprintf("%s/%s", clusterRoleBinding.RoleRef.Kind, clusterRoleBinding.RoleRef.Name))
-			needsRecreate = true
-		}
-
-		// Compare Subjects
-		// Note: This comparison assumes subjects are in the same order. If order differs, this will trigger a recreate.
-		if !needsRecreate && !cmp.Equal(existingCRB.Subjects, clusterRoleBinding.Subjects) {
-			r.Info("Subjects differ, will recreate",
-				"existing", existingCRB.Subjects,
-				"desired", clusterRoleBinding.Subjects)
-			needsRecreate = true
-		}
-
-		if needsRecreate {
-			r.Info("Deleting existing ClusterRoleBinding", "ClusterRoleBinding", clusterRoleBinding.Name, "shoot", shootName)
-			if err := shootClient.Delete(ctx, existingCRB); err != nil {
-				r.Error(err, "failed to delete existing ClusterRoleBinding", "ClusterRoleBinding", clusterRoleBinding.Name)
-				r.emitEvent(r.CareInstruction, corev1.EventTypeWarning, "RBACRecreationFailed",
-					fmt.Sprintf("Failed to recreate ClusterRoleBinding %s for shoot %s/%s: %v", clusterRoleBinding.Name, r.CareInstruction.Namespace, shootName, err))
-				return
-			}
-
-			r.Info("Creating new ClusterRoleBinding with correct configuration", "ClusterRoleBinding", clusterRoleBinding.Name, "shoot", shootName)
-			if err := shootClient.Create(ctx, clusterRoleBinding); err != nil {
-				r.Error(err, "failed to recreate ClusterRoleBinding", "ClusterRoleBinding", clusterRoleBinding.Name)
-				r.emitEvent(r.CareInstruction, corev1.EventTypeWarning, "RBACRecreationFailed",
-					fmt.Sprintf("Failed to recreate ClusterRoleBinding %s for shoot %s/%s: %v", clusterRoleBinding.Name, r.CareInstruction.Namespace, shootName, err))
-				return
-			}
-
-			r.Info("Successfully recreated ClusterRoleBinding", "ClusterRoleBinding", clusterRoleBinding.Name, "shoot", shootName)
-			r.emitEvent(r.CareInstruction, corev1.EventTypeNormal, "RBACUpdated",
-				fmt.Sprintf("Updated ClusterRoleBinding %s for shoot %s/%s", clusterRoleBinding.Name, r.CareInstruction.Namespace, shootName))
-		} else {
-			r.Info("ClusterRoleBinding matches desired state", "ClusterRoleBinding", clusterRoleBinding.Name, "shoot", shootName)
-		}
-	} else {
+	switch op {
+	case controllerutil.OperationResultCreated:
 		r.Info("Created ClusterRoleBinding for Greenhouse ServiceAccount", "ClusterRoleBinding", clusterRoleBinding.Name)
 		r.emitEvent(r.CareInstruction, corev1.EventTypeNormal, "RBACCreated",
 			fmt.Sprintf("Created ClusterRoleBinding %s for shoot %s/%s", clusterRoleBinding.Name, r.CareInstruction.Namespace, shootName))
+	case controllerutil.OperationResultUpdated:
+		r.Info("Updated ClusterRoleBinding for Greenhouse ServiceAccount", "ClusterRoleBinding", clusterRoleBinding.Name)
+		r.emitEvent(r.CareInstruction, corev1.EventTypeNormal, "RBACUpdated",
+			fmt.Sprintf("Updated ClusterRoleBinding %s for shoot %s/%s", clusterRoleBinding.Name, r.CareInstruction.Namespace, shootName))
+	case controllerutil.OperationResultNone:
+		r.Info("ClusterRoleBinding matches desired state", "ClusterRoleBinding", clusterRoleBinding.Name, "shoot", shootName)
 	}
 }
