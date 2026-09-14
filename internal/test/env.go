cc0174f9ffef210574f8ed9a567709989f6ccb7b
Piotr Skamruk <piotr.skamruk@gmail.com>
2026-09-14 12:15:47 +0200

    feat: move chart to the project repository
    Issue #74
    
    Github workflow for chart copied from permission-manager
    
    Signed-off-by: Piotr Skamruk <piotr.skamruk@gmail.com>

--- internal/test/env.go
+++ internal/test/env.go
@@ -127,7 +127,7 @@ func StartControlPlane(port string) (*rest.Config, client.Client, *envtest.Envir
 
 	testEnv.CRDDirectoryPaths = []string{
 		// shoot-grafter.cloudoperators
-		filepath.Join("..", "..", "crd"),
+		filepath.Join("..", "..", "charts", "shoot-grafter", "crds"),
 		// crds needed in testing:
 		// clusters.greenhouse.sap
 		// and a representation of github.com/gardener/gardener/pkg/apis/core/v1beta1/shoot as CRD
